d1ff4a46c7d2944804f43eb5abcb85c4472838b8
nickhargreaves <hargreaves1234@gmail.com>
2013-09-06 11:05:46 +0300

    initial commit with codeignited files

---
+++ README.md
@@ -0,0 +1,4 @@
+CodeIgnited
+==============
+
+A quick way to your get your CodeIgniter up with bootstrap themeing, admin interface and installer
---
+++ application/views/footer.php
@@ -0,0 +1,48 @@
+
+    <!-- Footer
+    ================================================== -->
+    <footer class="footer">
+      <div class="container">
+        <p>Designed and built with all the love in the world by <a href="http://twitter.com/mdo" target="_blank">@mdo</a> and <a href="http://twitter.com/fat" target="_blank">@fat</a>.</p>
+        <p>Code licensed under <a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">Apache License v2.0</a>, documentation under <a href="http://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>.</p>
+        <p><a href="http://glyphicons.com">Glyphicons Free</a> licensed under <a href="http://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>.</p>
+        <ul class="footer-links">
+          <li><a href="http://blog.getbootstrap.com">Blog</a></li>
+          <li class="muted">&middot;</li>
+          <li><a href="https://github.com/twitter/bootstrap/issues?state=open">Issues</a></li>
+          <li class="muted">&middot;</li>
+          <li><a href="https://github.com/twitter/bootstrap/blob/master/CHANGELOG.md">Changelog</a></li>
+        </ul>
+      </div>
+    </footer>
+
+
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+    <script src="<?php echo base_url();?>assets/js/jquery.js"></script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap-transition.js"></script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap-alert.js"></script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap-modal.js"></script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap-dropdown.js"></script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap-scrollspy.js"></script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap-tab.js"></script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap-tooltip.js"></script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap-popover.js"></script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap-button.js"></script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap-collapse.js"></script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap-carousel.js"></script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap-typeahead.js"></script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap-affix.js"></script>
+
+    <script src="<?php echo base_url();?>assets/js/holder/holder.js"></script>
+    <script src="<?php echo base_url();?>assets/js/google-code-prettify/prettify.js"></script>
+
+    <script src="<?php echo base_url();?>assets/js/application.js"></script>
+
+
+
+  </body>
+</html>
