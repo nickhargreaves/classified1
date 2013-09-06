ef446d6966c9ff51da280c3e5e346cb0d0a2a53b
nickhargreaves <hargreaves1234@gmail.com>
2013-09-06 11:19:53 +0300

    gitignore

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/CodeIgnited/';
+$config['base_url']	= 'http://localhost/classified1/';
 
 /*
 |--------------------------------------------------------------------------
